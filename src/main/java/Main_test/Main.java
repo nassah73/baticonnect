119f583b48fb1cf3a0e5c73800bba291a3834261
hassan <hassansobai6@gmail.com>
2025-12-01 17:18:31 +0100

    javafx structure

--- src/main/java/Main_test/Main.java
+++ src/main/java/Main_test/Main.java
@@ -1,18 +1,31 @@
 package Main_test;
-import client.*;
-import database_managment.*;
-public class Main {
-    public static void main(String[] args) {
-      /*   paymentProject testPayment = new paymentProject(1, 5000.0, "2024-06-15", "Credit Card", "hass@11", 101, 15000.0, 2000.0);
-        System.out.println("Payment ID: " + testPayment.getPaymentId());
-        System.out.println("Amount: " + testPayment.getAmount());
-        System.out.println("Payment Date: " + testPayment.getPaymentDate());
-        System.out.println("Payment Method: " + testPayment.getPaymentMethod());
-        testPayment.pay();
-        testPayment.displayPaymentInfo();*/
-     Client client = new Client(1,"Doe", "John", "kkk",30,
-             "123 Main St", "555-1234", "password123",
-             1000.0f, "New York");
-        System.out.println(client);
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Label helloLabel = new Label("🥳 JavaFX Khdama! Baticonnect Is Ready.");
+
+
+        StackPane root = new StackPane();
+        root.getChildren().add(helloLabel);
+
+
+        Scene scene = new Scene(root, 400, 300);
+
+
+        stage.setTitle("JavaFX Test - Baticonnect");
+        stage.setScene(scene);
+        stage.show();
     }
-}//int paymentId, double amount, String paymentDate, String paymentMethod,String clientEmail, int projectId, double projectBudget, double solde
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
